92cd4d79aac657576f306076245189d7efe152c7
Artem Khan <akhan195@my.bcit.ca>
2022-07-07 16:12:57 -0700

    deployment trial #2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001;
 
 const io = new Server(server, {
     cors: {
-        origin: "https://artemkhn.github.io/Personal_Chat_Project",
+        origin: "http://localhost:3000",
         methods: ["GET", "POST"],
     },
 });
